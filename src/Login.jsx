540dcb65ec0af0fbd0eabbe9dcf9594e4f76f084
Tobias M <tobiasmautner@outlook.com>
2026-08-02 12:09:23 +0200

    s

---
+++ src/Login.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react'
+import { supabase } from './data/Export/Supabase.jsx'
+
+const Login = () => {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {(  name="Emilia" && password==="Emilia123" )
+        navigate("/emilia")
+             if (error) throw error
+      console.log('Logged in user:', user)
+    } catch (error) {
+      console.error('Error logging in:', error.message)
+    }navigate("/emlia")
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
